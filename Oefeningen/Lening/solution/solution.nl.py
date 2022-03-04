58d9a36d7ce9933ba3fe112f9b51d8c599be1ac5
blommejonas <69513699+blommejonas@users.noreply.github.com>
2022-03-04 22:10:16 +0100

    Update solution.nl.py

--- Oefeningen/Lening/solution/solution.nl.py
+++ Oefeningen/Lening/solution/solution.nl.py
@@ -1,14 +1,11 @@
 V = float(input("Het te ontlenen bedrag : "))
-r = float(input("De jaarlijkse rentevoet : "))
-N = float(input("Over hoeveel jaar loopt de lening : "))
+r = float(input("De jaarlijkse rentevoet (in %) : "))
+n = int(input("Over hoeveel maanden loopt de lening : "))
 
-n = N * 12
 i = (1+r/100)**(1/12) - 1
 A = (i*V)/(1-(1+i)**(-n))
 totaal = A * n
 
-print("n = " + str(int(n)) + " maanden")
-print("i = " + str(round(i*100,3)) + " %")
+print("i = " + str(round(i*100,4)) + " %")
 print("A = " + str(round(A,2)) + " euro")
-print("Totaal af te lossen bedrag = " + str(round(totaal,2)))
-print("Totaal bedrag intresten    = " + str(round(totaal-V,2)))
+print("V_tot = " + str(round(totaal,2)) + " euro")
