2738401bdb534768e56b291bc72bb60cce64cae1
blommejonas <69513699+blommejonas@users.noreply.github.com>
2022-07-07 23:39:25 +0200

    Update solution.nl.py

--- Oefeningen/Palindroom/solution/solution.nl.py
+++ Oefeningen/Palindroom/solution/solution.nl.py
@@ -1,11 +1,11 @@
 woord = input("Geef een woord: ")
 omgekeerd = ""
-palindroom = "Dit is geen palindroom"
+palindroom = "Dit is geen palindroom."
 
 for i in woord:
     omgekeerd = i + omgekeerd
 
 if woord == omgekeerd:
-    palindroom = "Dit is een palindroom"
+    palindroom = "Dit is een palindroom."
 
 print(woord,"-",omgekeerd,"-",palindroom)
